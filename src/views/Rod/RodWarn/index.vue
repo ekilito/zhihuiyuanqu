40450d85fc6f1078398023998fa8606fef8a5640
ekilito <2856509748@qq.com>
2023-08-29 21:42:01 +0800

    告警完成

--- src/views/Rod/RodWarn/index.vue
+++ src/views/Rod/RodWarn/index.vue
@@ -3,9 +3,9 @@
     <!-- 搜索区域 -->
     <div class="search-container">
       <span class="search-label">一体杆名称：</span>
-      <el-input placeholder="请输入一体杆名称" class="search-main" />
+      <el-input v-model="params.poleName" clearable placeholder="请输入一体杆名称" class="search-main" @clear="doSearch" />
       <span class="search-label">一体杆编号：</span>
-      <el-input clearable placeholder="请输入一体杆编号" class="search-main" />
+      <el-input v-model="params.poleNumber" clearable placeholder="请输入一体杆编号" class="search-main" @clear="doSearch" />
       <span class="search-label">处置状态：</span>
 
       <!--
@@ -14,63 +14,68 @@
           label（中文显示）
           value （选中之后赋值给v-model的数据将来传给后端）
      -->
-      <!--  <el-select>
+      <el-select v-model="params.handleStatus">
         <el-option
-          v-for="item in payStatusList"
+          v-for="item in warnStatusList"
           :key="item.id"
           :value="item.id"
           :label="item.name"
         />
-      </el-select> -->
-      <el-button type="primary" style="margin-left: 18px;">查询</el-button>
+      </el-select>
+      <el-button type="primary" style="margin-left: 18px;" @click="doSearch">查询</el-button>
     </div>
     <!-- 表格区域 -->
     <div class="table">
       <el-table
         style="width: 100%"
-        :data="carpayList"
+        :data="warnList"
       >
         <el-table-column
           type="index"
           label="序号"
+          width="80"
         />
         <el-table-column
-          label="车牌号码"
+          label="一体杆名称"
           width="180"
-          prop="carNumber"
+          prop="poleName"
         />
         <el-table-column
-          label="收费类型"
-          prop="chargeType"
+          label="一体杆编号"
+          prop="poleNumber"
+        />
+        <el-table-column
+          label="故障类型"
+          width="120"
+          prop="errorType"
+        />
+        <el-table-column
+          label="处置状态"
+          prop="handleStatus"
         ><template #default="scope">
-          {{ formatStatusType(scope.row.chargeType) }}
+          {{ formatStatus(scope.row.handleStatus) }}
         </template>
         </el-table-column>
         <el-table-column
-          label="停车总时常"
-          width="120"
-          prop="parkingTime"
+          label="告警时间"
+          prop="warningTime"
         />
-        <el-table-column
-          label="缴纳费用(元)"
-          width="120"
-          prop="actualCharge"
-        />
-        <el-table-column
-          label="缴纳状态"
-          prop="paymentStatus"
-        ><template #default="scope">
-          {{ formatStatus(scope.row.paymentStatus) }}
-        </template>
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button
+              size="mini"
+              type="text"
+            >派单</el-button>
+            <el-button
+              size="mini"
+              type="text"
+            >详情</el-button>
+            <el-button
+              size="mini"
+              type="text"
+            >删除</el-button>
+          </template>
         </el-table-column>
-        <el-table-column
-          label="缴纳方式"
-          prop="paymentMethod"
-        />
-        <el-table-column
-          label="缴纳时间"
-          prop="paymentTime"
-        />
       </el-table>
 
     </div>
@@ -78,7 +83,7 @@
     <div class="page-container">
       <el-pagination
         :current-page="params.page"
-        layout="total, sizes,prev, pager, next"
+        layout="total,prev, pager, next"
         :page-sizes="[10, 20, 50, 100]"
         :page-size="params.pageSize"
         :total="total"
@@ -90,75 +95,83 @@
 </template>
 
 <script>
-import { getCarPayListAPI } from '@/api/carpay'
+import { getWarningListAPI } from '@/api/warning'
 export default {
-  name: 'CarPay',
+  name: 'RodWarn',
   data() {
     return {
-      carpayList: [],
+      warnList: [],
       params: { // 请求参数
         page: 1,
-        pageSize: 10,
-        carNumber: '',
-        paymentStatus: null
+        pageSize: 4,
+        poleName: '',
+        poleNumber: '',
+        handleStatus: null
       },
       total: 0,
-      // 月卡状态
-      payStatusList: [
+      // 处置状态 0:未派单,1:已派单,2:已接单,3:已完成
+      warnStatusList: [
         {
           id: null,
           name: '全部'
         },
         {
           id: 0,
-          name: '未缴费'
+          name: '未派单'
         },
         {
           id: 1,
-          name: '已缴费'
+          name: '已派单'
+        },
+        {
+          id: 2,
+          name: '已接单'
+        },
+        {
+          id: 3,
+          name: '已完成'
         }
       ]
+
     }
   },
   created() {
-    this.getCarPayList()
+    this.getWarningList()
   },
   methods: {
-    async getCarPayList() {
-      const res = await getCarPayListAPI(this.params)
-      this.carpayList = res.data.rows
+    async getWarningList() {
+      const res = await getWarningListAPI(this.params)
+      this.warnList = res.data.rows
       // console.log(this.carpayList)
       this.total = res.data.total
+      //  console.log(this.total)
     },
     formatStatus(status) {
       const statusMap = {
-        0: '未缴纳',
-        1: '已缴纳'
+        0: '未派单',
+        1: '已派单',
+        2: '已接单',
+        3: '已完成'
       }
       return statusMap[status]
     },
-    formatStatusType(type) {
-      if (type === 'card') {
-        return '月卡'
-      } else if (type === 'temp') {
-        return '临时停车'
-      }
-    },
+    // 分页
     handleSizeChange(val) {
       this.params.pageSize = val
-      this.getCarPayList()
+      this.getWarningList()
       // console.log(`每页 ${val} 条`)
     },
 
     pageChange(page) {
       this.params.page = page
-      this.getCarPayList()
+      this.getWarningList()
     },
+    // 模糊搜索
     doSearch() {
       // 1. 重置page为1
       this.params.page = 1
       // 2. 调用获取列表接口
-      this.getCarPayList()
+      this.getWarningList()
     }
 
   }
